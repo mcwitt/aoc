e91ee36a6b12688498c43036ec12ad27177f9ea1
Matt Wittmann <mcwitt@gmail.com>
2025-12-04 14:06:12 -0800

    Day 4: comonad

--- app/D04.hs
+++ app/D04.hs
@@ -1,50 +1,49 @@
 module Main where
 
 import Control.Arrow ((&&&), (>>>))
-import Data.List
+import Control.Comonad.Store
+import Data.List (unfoldr)
+import Data.Set (Set)
+import Data.Set qualified as Set
 
-parse = lines
+type Coord = (Int, Int)
 
-type Tile = Char
+type Grid = Store Coord
 
-type Grid a = [[a]]
+parse :: String -> ((Int, Int), Set Coord)
+parse s =
+  let xss = lines s
+   in ( (length xss, length (head xss)),
+        Set.fromList
+          [ (r, c)
+          | (r, xs) <- zip [1 ..] xss,
+            (c, '@') <- zip [1 ..] xs
+          ]
+      )
 
-windows :: Int -> [a] -> [[a]]
-windows k = foldr (zipWith (:)) (repeat []) . take k . tails
+solve1 (dims, coords) =
+  let grid = store (`Set.member` coords) (0, 0)
+      accessible = extend rule grid
+   in length $ filter (\c -> peek c grid && peek c accessible) $ allCoords dims
 
-windowsGrid :: Int -> Grid a -> Grid (Grid a)
-windowsGrid k = map (map transpose . windows k . transpose) . windows k
+allCoords (h, w) = [(r, c) | r <- [1 .. h], c <- [1 .. w]]
 
-pad :: a -> [a] -> [a]
-pad empty xs = empty : xs ++ [empty]
-
-padGrid :: a -> Grid a -> Grid a
-padGrid empty xss =
-  let w = length (head xss)
-   in pad (replicate (w + 2) empty) $ map (pad empty) $ xss
-
-accessible :: Grid Tile -> Grid Bool
-accessible = (map . map) kernel . windowsGrid 3 . padGrid '.'
+rule :: Grid Bool -> Bool
+rule = (< 5) . length . filter id . experiment neighborhood
   where
-    kernel xss@[_, [_, x, _], _] = x == '@' && length (filter ('@' ==) (concat xss)) < 5
+    neighborhood (r, c) = [(r + dr, c + dc) | dr <- [-1 .. 1], dc <- [-1 .. 1]]
 
-solve1 :: Grid Tile -> Int
-solve1 = length . filter id . concat . accessible
+step :: (Int, Int) -> Set Coord -> Maybe (Int, Set Coord)
+step dims coords =
+  let grid = store (`Set.member` coords) (0, 0)
+      accessible = extend rule grid
+      nextCoords = Set.fromList $ filter (\c -> peek c grid && not (peek c accessible)) $ allCoords dims
+      numRemoved = length coords - length nextCoords
+   in if numRemoved == 0
+        then Nothing
+        else Just (numRemoved, nextCoords)
 
-zipWithGrid :: (a -> b -> c) -> Grid a -> Grid b -> Grid c
-zipWithGrid = zipWith . zipWith
-
-removeAccessible :: Grid Tile -> Grid Tile
-removeAccessible xss = zipWithGrid rule xss (accessible xss)
-  where
-    rule _ True = '.'
-    rule prev False = prev
-
-solve2 :: Grid Char -> Int
-solve2 xss = countPaper xss - countPaper xss'
-  where
-    xss' = head . dropWhile ((> 0) . solve1) . iterate removeAccessible $ xss
-    countPaper = length . filter ('@' ==) . concat
+solve2 (dims, grid) = sum $ unfoldr (step dims) grid
 
 main :: IO ()
 main = interact (parse >>> (solve1 &&& solve2) >>> show)
